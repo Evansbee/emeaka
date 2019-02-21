c498d6a5b19215553646ed4b9b9253b33314262f
Evan Ackmann <evansbee@gmail.com>
2019-02-21 11:50:42 -0500

    working on font thing

--- fontmaker/main.cpp
+++ fontmaker/main.cpp
@@ -8,21 +8,26 @@ int main(int argc, char **argv)
 {
    stbtt_fontinfo font;
    unsigned char *bitmap;
-   int w,h,xoff,yoff,i,j,c = (argc > 1 ? (argv[1][0]) : 'a'), s = (argc > 2 ? atoi(argv[2]) : 20);
+   int w,h,xoff,yoff,i,j,c, s = (argc > 1 ? atoi(argv[1]) : 20);
    fread(ttf_buffer, 1, 1<<25, fopen(argc > 3 ? argv[3] : "DejaVuSans.ttf", "rb"));
 
-    for(int c = 0x20; c <= 0xFE; ++c)
+    for(c = 0x20; c <= 0x7E; ++c)
     {
-
-   stbtt_InitFont(&font, ttf_buffer, stbtt_GetFontOffsetForIndex(ttf_buffer,0));
-   bitmap = stbtt_GetCodepointBitmap(&font, 0,stbtt_ScaleForPixelHeight(&font, s), c, &w, &h, &xoff,&yoff);
+            stbtt_InitFont(&font, ttf_buffer, stbtt_GetFontOffsetForIndex(ttf_buffer,0));
+    float SF = stbtt_ScaleForPixelHeight(&font, s);
+    bitmap = stbtt_GetCodepointBitmap(&font, 0, SF, c, &w, &h, &xoff,&yoff);
    
    int ascent, descent, linegap;
    stbtt_GetFontVMetrics(&font,&ascent, &descent, &linegap);
 
-    int advance, leftside;
+int advance, leftside;
    stbtt_GetCodepointHMetrics(&font,c,&advance,&leftside);
+    advance = advance * SF;
 
+    
+
+printf("Code point: %c\n",c);
+printf("w: %d h: %d xoff: %d yoff: %d advance: %d\n",w,h,xoff,yoff,advance);
    for (j=0; j < h; ++j) {
       for (i=0; i < w; ++i)
          putchar(" .:ioVM@"[bitmap[j*w+i]>>5]);
